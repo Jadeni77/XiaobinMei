8ea11162b6f409a9c644c844dfde36db6060b8c4
XiaobinMei <mei.xiaob@northeastern.edu>
2025-06-28 15:31:43 -0400

    Implement the Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function Hero() {
+  return (
+    <section id="home" className="hero">
+      <div className="container hero-container">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            Hi, I'm <span className="highlight">Xiaobin Mei</span>
+          </h1>
+          <h2 className="hero-subtitle">Frontend Developer</h2>
+          <p className="hero-description">
+            I am studying in Northeastern University
+          </p>
 
+          <div className="here-button">
+            <a href="#projects" className="btn btn-primary">
+              View My Work
+            </a>
+            <a href="#contact" className="btn btn-secondary">
+              Contact Me
+            </a>
+          </div>
+        </div>
+
+        <div className="hero-image-container">
+          <div className="hero-image">
+            <div className="hero-image-placeholder">....</div>
+              <div className="badge">Open to Work!</div>
+            </div>
+          </div>
+        </div>
+
+    </section>
+  );
 }
 
 export default Hero;
