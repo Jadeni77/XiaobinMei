dfcba69de312d8cfe13784bb0d22ce522ff26c9f
XiaobinMei <mei.xiaob@northeastern.edu>
2025-06-28 20:37:07 -0400

    Update to include my linkedin image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,9 @@ function Hero() {
           </h1>
           <h2 className="hero-subtitle">Frontend Developer</h2>
           <p className="hero-description">
-            I am studying in Northeastern University
+            I create engaging, responsive websites with modern JavaScript
+            frameworks. Passionate about clean code and intuitive user
+            experiences.
           </p>
 
           <div className="here-button">
@@ -25,12 +27,16 @@ function Hero() {
 
         <div className="hero-image-container">
           <div className="hero-image">
-            <div className="hero-image-placeholder">....</div>
-              <div className="badge">Open to Work!</div>
+            <div className="hero-image-placeholder">
+                <img src="https://media.licdn.com/dms/image/v2/D4D03AQGqhZ6j96dmnQ/profile-displayphoto-shrink_800_800/B4DZVtvWHkHYAc-/0/1741302883892?e=1756944000&v=beta&t=m2tPR5TXL58QibvAEbTmOnD7e4LvpzWluFpDshbb9Ao"
+                alt="Xiaobin Mei"
+                className="profile-image"
+                />
             </div>
+            <div className="badge">Open to Work!</div>
           </div>
         </div>
-
+      </div>
     </section>
   );
 }
